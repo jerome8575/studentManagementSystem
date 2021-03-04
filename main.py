bc0d85e06bd0dda92faa66028132fcca18060d95
Jerome Emery <58118521+jerome8575@users.noreply.github.com>
2021-03-04 18:22:04 -0500

    Update main.py
    Fixed done button after adding a class.

--- main.py
+++ main.py
@@ -1,95 +1,106 @@
 import PySimpleGUI as sg
-from Student import Student, Students
+
 from Class import Class
+from Student import Student, Students
 
 # creates a list of all students
 studentList = Students()
+
 
 # creates a class object and adds it to a class list
 def createClass(classEvent, studentClasses):
     cl = Class(classEvent)
     studentClasses.append(cl)
 
+
 # checks if a window was closed or exited
 def checkClosedWindow(event):
     if event == sg.WIN_CLOSED or event == "Done":
         return True
 
+
 # reads the window for selected class subject
 def readSubjectWindow(subjectEvent, studentClasses):
-    if subjectEvent == "Math":
-        event, values = windowMath.read()
-        if checkClosedWindow(event):
-            windowMath.close()
-        elif event == "MAT101":
-            createClass(event, studentClasses)
-    elif subjectEvent == "English":
-        event, values = windowEnglish.read()
-        if checkClosedWindow(event):
-            windowEnglish.close()
-        elif event != None:
-            createClass(event, studentClasses)
-    elif subjectEvent == "Science":
-        event, values = windowScience.read()
-        if checkClosedWindow(event):
-            windowScience.close()
-        elif event != None:
-            createClass(event, studentClasses)
-    elif subjectEvent == "Language":
-        event, values = windowLanguage.read()
-        if checkClosedWindow(event):
-            windowLanguage.close()
-        elif event != None:
-            createClass(event, studentClasses)
-    elif subjectEvent == "History":
-        event, values = windowHistory.read()
-        if checkClosedWindow(event):
-            windowHistory.close()
-        elif event != None:
-            createClass(event, studentClasses)
-    elif subjectEvent == "Electives":
-        event, values =windowElective.read()
-        if checkClosedWindow(event):
-            windowElective.close()
-        elif event != None:
-            createClass(event, studentClasses)
+    while True:
+        if subjectEvent == "Math":
+            event, values = windowMath.read()
+            if checkClosedWindow(event):
+                windowMath.close()
+                break
+            elif event == "MAT101":
+                createClass(event, studentClasses)
+        elif subjectEvent == "English":
+            event, values = windowEnglish.read()
+            if checkClosedWindow(event):
+                windowEnglish.close()
+                break
+            elif event != None:
+                createClass(event, studentClasses)
+        elif subjectEvent == "Science":
+            event, values = windowScience.read()
+            if checkClosedWindow(event):
+                windowScience.close()
+                break
+            elif event != None:
+                createClass(event, studentClasses)
+        elif subjectEvent == "Language":
+            event, values = windowLanguage.read()
+            if checkClosedWindow(event):
+                windowLanguage.close()
+                break
+            elif event != None:
+                createClass(event, studentClasses)
+        elif subjectEvent == "History":
+            event, values = windowHistory.read()
+            if checkClosedWindow(event):
+                windowHistory.close()
+                break
+            elif event != None:
+                createClass(event, studentClasses)
+        elif subjectEvent == "Electives":
+            event, values = windowElective.read()
+            if checkClosedWindow(event):
+                windowElective.close()
+                break
+            elif event != None:
+                createClass(event, studentClasses)
 
 
 # basic layout of the first window
 layoutStudentInfo = [[sg.Text("Add a student")],
-          [sg.Text("First Name  "), sg.InputText('', size=(10, 1), key="fn")],
-          [sg.Text("Last Name   "), sg.InputText('', size=(10, 1), key="ln")],
-          [sg.Text("Grade       "), sg.InputText('', size=(10,1), key="gr")],
-          [sg.Button("Add Classes")],
-          [sg.Button("OK"), sg.Button("Quit")]]
+                     [sg.Text("First Name  "), sg.InputText('', size=(10, 1), key="fn")],
+                     [sg.Text("Last Name   "), sg.InputText('', size=(10, 1), key="ln")],
+                     [sg.Text("Grade       "), sg.InputText('', size=(10, 1), key="gr")],
+                     [sg.Button("Add Classes")],
+                     [sg.Button("OK"), sg.Button("Quit")]]
 # this window asks user to click on the class subject they want to add
 layoutAddClassSubject = [[sg.Text("Select subject")],
-                    [sg.Button("Math"), sg.Button("English"), sg.Button("Science")],
-                    [sg.Button("Language"), sg.Button("History"), sg.Button("Electives")],
-                    [sg.Button("Done")]]
+                         [sg.Button("Math"), sg.Button("English"), sg.Button("Science")],
+                         [sg.Button("Language"), sg.Button("History"), sg.Button("Electives")],
+                         [sg.Button("Done")]]
 # windows for each class subject
 layoutMath = [[sg.Text("MATH")],
               [sg.Button("MAT101")],
               [sg.Button("Done")]]
 layoutEnglish = [[sg.Text("ENGLISH")],
-              [sg.Button("ENG101")],
-              [sg.Button("Done")]]
+                 [sg.Button("ENG101")],
+                 [sg.Button("Done")]]
 layoutScience = [[sg.Text("SCIENCE")],
-              [sg.Button("SCI101")],
-              [sg.Button("Done")]]
+                 [sg.Button("SCI101")],
+                 [sg.Button("Done")]]
 layoutLanguage = [[sg.Text("LANGUAGE")],
-              [sg.Button("LANG101")],
-              [sg.Button("Done")]]
+                  [sg.Button("LANG101")],
+                  [sg.Button("Done")]]
 layoutHistory = [[sg.Text("HISTORY")],
-              [sg.Button("HIS101")],
-              [sg.Button("Done")]]
+                 [sg.Button("HIS101")],
+                 [sg.Button("Done")]]
 layoutElective = [[sg.Text("ELECTIVE")],
-              [sg.Button("ELECT101")],
-              [sg.Button("Done")]]
+                  [sg.Button("ELECT101")],
+                  [sg.Button("Done")]]
 
 # create windows
 windowStudentInfo = sg.Window("student", layoutStudentInfo, margins=(50, 50))
-windowAddClassSubject = sg.Window("classes", layoutAddClassSubject, margins=(30,30))
+windowAddClassSubject = sg.Window("classes", layoutAddClassSubject, margins=(30, 30))
 windowMath = sg.Window("math", layoutMath)
 windowEnglish = sg.Window("english", layoutEnglish)
 windowScience = sg.Window("science", layoutScience)
@@ -115,7 +126,6 @@ while True:
     elif event == "OK":
         # create student object with input from user as fields
         student = Student(values["fn"], values["ln"], int(values["gr"]), studentClasses)
-        print(studentClasses)
         studentList.addStudent(student)
 
     elif event == sg.WIN_CLOSED or event == "Quit":

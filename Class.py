e978db6b6b943bb9a4fa79e1c7978e3c5b95c3eb
Jerome Emery <58118521+jerome8575@users.noreply.github.com>
2021-03-04 18:20:50 -0500

    Update Class.py
    Fixed printing the class schedule.

--- Class.py
+++ Class.py
@@ -1,9 +1,8 @@
 class Class:
-
     className = ""
 
     def __init__(self, name):
         self.className = name
 
     def printClass(self):
-        print(self.className)
+        print(self.className, "  ", end='')
